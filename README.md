9d747a932287b474d53ef89dd8ad69b2388bd8d9
rahul-thakoor <rahul.thakoor@gmail.com>
2019-09-04 16:21:23 +0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Express + Local Instance of MongoDB
+
+- Express app based on https://zellwk.com/blog/crud-express-mongodb/
---
+++ express/server.js
@@ -0,0 +1,69 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const MongoClient = require("mongodb").MongoClient;
+
+var db;
+
+MongoClient.connect(
+  `mongodb://${process.env.MD_USER}:${process.env.MD_PASS}@localhost:27017/star-wars-quotes`,
+  (err, database) => {
+    if (err) return console.log(err);
+    db = database.db("star-wars-quotes");
+    app.listen(process.env.PORT || 3000, () => {
+      console.log("listening on 3000");
+    });
+  }
+);
+
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  db.collection("quotes")
+    .find()
+    .toArray((err, result) => {
+      if (err) return console.log(err);
+      res.render("index.ejs", { quotes: result });
+    });
+});
+
+app.post("/quotes", (req, res) => {
+  db.collection("quotes").save(req.body, (err, result) => {
+    if (err) return console.log(err);
+    console.log("saved to database");
+    res.redirect("/");
+  });
+});
+
+app.put("/quotes", (req, res) => {
+  db.collection("quotes").findOneAndUpdate(
+    { name: "Yoda" },
+    {
+      $set: {
+        name: req.body.name,
+        quote: req.body.quote
+      }
+    },
+    {
+      sort: { _id: -1 },
+      upsert: true
+    },
+    (err, result) => {
+      if (err) return res.send(err);
+      res.send(result);
+    }
+  );
+});
+
+app.delete("/quotes", (req, res) => {
+  db.collection("quotes").findOneAndDelete(
+    { name: req.body.name },
+    (err, result) => {
+      if (err) return res.send(500, err);
+      res.send("A darth vadar quote got deleted");
+    }
+  );
+});
