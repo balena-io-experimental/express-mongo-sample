f84dee5e98d31cf55f2065fcfb2dd90ba97d6b59
rahul-thakoor <rahul.thakoor@gmail.com>
2019-09-05 13:55:32 +0400

    create chart with sample data

--- express/public/main.js
+++ express/public/main.js
@@ -1,34 +1,66 @@
-/* globals fetch */
-var update = document.getElementById('update')
-var del = document.getElementById('delete')
+Vue.component("line-chart", {
+  extends: VueChartJs.Line,
+  mixins: [VueChartJs.mixins.reactiveProp],
+  props: ["options"],
+  mounted() {
+    this.renderChart(this.chartData, this.options);
+  }
+});
 
-update.addEventListener('click', function () {
-  fetch('quotes', {
-    method: 'put',
-    headers: {'Content-Type': 'application/json'},
-    body: JSON.stringify({
-      'name': 'Darth Vader',
-      'quote': 'I find your lack of faith disturbing.'
-    })
-  })
-  .then(response => {
-    if (response.ok) return response.json()
-  })
-  .then(data => {
-    console.log(data)
-  })
-})
-
-del.addEventListener('click', function () {
-  fetch('quotes', {
-    method: 'delete',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({
-      'name': 'Darth Vader'
-    })
-  }).then(function (response) {
-    window.location.reload()
-  })
-})
+var vm = new Vue({
+  el: "#app",
+  data: function() {
+    return {
+      options: {
+        responsive: true,
+        maintainAspectRatio: false,
+        scales: {
+          xAxes: [
+            {
+              type: "time",
+              distribution: "series",
+              ticks: {
+                source: "data",
+                autoSkip: true
+              },
+              time: {
+                unit: "minute",
+                stepSize: 2,
+                tooltipFormat: "lll",
+                displayFormats: {
+                  hour: "HH:mm:ss"
+                }
+              },
+              display: true
+            }
+          ]
+        }
+      },
+      datacollection: {
+        labels: [
+          "2019-09-05T09:01:54.607Z",
+          "2019-09-05T09:03:54.607Z",
+          "2019-09-05T09:04:54.607Z",
+          "2019-09-05T09:05:54.607Z",
+          "2019-09-05T09:06:54.607Z",
+          "2019-09-05T09:11:54.607Z",
+          "2019-09-05T09:12:54.607Z"
+        ],
+        datasets: [
+          {
+            label: "Temperature",
+            backgroundColor: "#f87979",
+            data: [30, 45, 10, 40, 39, 80, 40],
+            fill: false
+          }
+        ]
+      }
+    };
+  },
+  mounted() {
+    this.fillData();
+  },
+  methods: {
+    fillData: function() {}
+  }
+});
