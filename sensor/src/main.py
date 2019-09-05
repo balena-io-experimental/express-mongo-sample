ed44bfb1339cc72dd77cf586978bb9b2a2be8c23
Daniel Andrade <daniel@danielandrade.net>
2019-09-05 10:06:50 +0100

    Add weather info

--- sensor/src/main.py
+++ sensor/src/main.py
@@ -1,17 +1,36 @@
 import time
+import random
+import json
+from datetime import datetime
 from pymongo import MongoClient
 from pprint import pprint
+from urllib.request import urlopen
 
-client = MongoClient('mongodb://localhost:27017/sensorData')
+def readWeather(code, apikey):
+    # apikey="6b13bbc2262eedc8baecf752c9940d9c" # sign up here https://home.openweathermap.org/api_keys
+
+    url="https://api.openweathermap.org/data/2.5/weather?q="+code+"&appid="+apikey
+
+    meteo = urlopen(url).read()
+    meteo = meteo.decode('utf-8')
+    weather = json.loads(meteo)
+
+    return weather
+
+
+client = MongoClient('localhost',27017)
 
 with client:
     db = client.sensorData
+    col = db.data
 
     while True:
-        print("Hi!")
-        print(db.list_database_names())
+        current_time = datetime.now()
+        weather = readWeather("lisbon,pt","6b13bbc2262eedc8baecf752c9940d9c")
 
-        status = db.command("serverStatus")
-        pprint(status)
+        read = { "time": current_time, "weather": weather }
+        x = col.insert_one(read)
 
-        time.sleep(2)
+        print("Data inserted...")
+
+        time.sleep(10)
